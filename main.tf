d122ae543d7d3c60b9e155bfea8fb9fc6e893d36
mccreed13 <103462761+mccreed13@users.noreply.github.com>
2024-05-28 18:35:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,70 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
+terraform{
+ required_providers {
+  aws = {
+   source = "hashicorp/aws"
+   version = "~> 4.16"
   }
-
-  required_version = ">= 1.2.0"
+ }
 }
 
 provider "aws" {
-  region  = "eu-north-1"
+ region = "eu-north-1"
+ access_key = "AKIAW3MEBMAK2QVGOSXK"
+ secret_key = "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"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0914547665e6a707c"
-  instance_type = "t3.micro"
+resource "aws_instance" "web" {
+ ami = "ami-0705384c0b33c194c"
+ instance_type = "t3.micro"
+ key_name = "keylab4"
+ vpc_security_group_ids = [aws_security_group.http_server.id]
+ 
+ tags = {
+  Name = "Lab6"
+ }
+ 
+ user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y
+  sudo apt install -y docker
+  sudo docker pull mccreed13/iitlab4:latest
+  sudo docker rm -f iitlab4-containter || true
+  sudo docker run -d -p 80:80 --name iitlab4-containter mccreed13/iitlab4
+ EOF 
+}
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+resource "aws_security_group" "http_server"{
+ name = "web_server"
+ description = "Inbound and outbound traffic"
+ vpc_id = "vpc-06412e926c584167b"
+ 
+ tags = {
+  Name = "web-server"
+ }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_server_http_rule" {
+ security_group_id = aws_security_group.http_server.id
+ cidr_ipv4 = "0.0.0.0/0"
+ from_port = 80
+ ip_protocol = "tcp"
+ to_port = 80
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "http_server_ssh_rule" {
+ security_group_id = aws_security_group.http_server.id
+ cidr_ipv4 = "0.0.0.0/0"
+ from_port = 22
+ ip_protocol = "tcp"
+ to_port = 22
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "http_server_outbound_rule" {
+ security_group_id = aws_security_group.http_server.id
+ from_port = 0
+ to_port = 0
+ cidr_ipv4 = "0.0.0.0/0"
+ ip_protocol = "-1"
 }
